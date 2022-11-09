531f1ccae7d8df45be087bdad88cb155a87aca6f
guswlsakfls <guswlsakfls@gmail.com>
2022-11-09 15:08:46 +0900

    Style: 코드 수정 없

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -2,12 +2,5 @@ export const dbConfig = {
     HOST: "127.0.0.1",
     USER: "root",
     PASSWORD: "111111",
-    DB: "cmarket"
+    DB: "korep"
 }
-
-// export const dbConfig = {
-//     HOST: "127.0.0.1",
-//     USER: "root",
-//     PASSWORD: "111111",
-//     DB: "cmarket"
-// }
